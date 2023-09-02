a6f793b607103574f8a40ca0c448d943207406be
Maverick18 <115613425+Aditya-Shandiya1812@users.noreply.github.com>
2023-09-03 00:23:10 +0530

    frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import TextEditor from './TextEditor';
+import TextEditor from "./TextEditor"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
 
 function App() {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element= {<Navigate to={`/documents/${uuidV4()}`} />}>
+          </Route>
+        <Route path="/documents/:id" element= {<TextEditor />}>
+          
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
-export default App;
+export default App
